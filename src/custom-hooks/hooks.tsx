8d8c0932f5efb873113aaafdb69ef282fac751c0
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-30 17:21:51 -0600

    add custom hooks to fetch products and product

---
+++ src/custom-hooks/hooks.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+
+type TProduct = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+};
+
+const initialState: TProduct[] = [];
+
+export const fetchProducts = () => {
+  const [products, setProducts] = useState<TProduct[]>();
+  const [status, setStatus] = useState<'idle' | 'loading' | 'error'>('idle');
+
+  useEffect(() => {
+    const getData = async () => {
+      setStatus('loading');
+      try {
+        const res = await fetch('https://fakestoreapi.com/products');
+        const data: TProduct[] = await res.json();
+        setStatus('idle');
+        setProducts(data);
+      } catch (e) {
+        console.error(e);
+        setStatus('error');
+      }
+    };
+    getData();
+  }, []);
+  return { products, status };
+};
+
+export const fetchProduct = (id: string) => {
+  const [product, setProduct] = useState<TProduct>();
+  const [status, setStatus] = useState<'idle' | 'loading' | 'error'>('idle');
+
+  useEffect(() => {
+    const getData = async () => {
+      setStatus('loading');
+      try {
+        const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const data: TProduct = await res.json();
+        setStatus('idle');
+        setProduct(data);
+      } catch (e) {
+        console.error(e);
+        setStatus('error');
+      }
+    };
+    getData();
+  }, [id]);
+  return { product, status };
+};
