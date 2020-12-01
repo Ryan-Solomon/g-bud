6acc09374c219846d297695b88927a570f312c82
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-30 21:00:13 -0600

    add cart layout

--- src/cart/Cart.tsx
+++ src/cart/Cart.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import './Cart.styles.scss';
 
 const Cart = () => {
-  return <div>This is a cart. It's used for items</div>;
+  return (
+    <main>
+      <header>Your Cart</header>
+      <section className='items-container'>
+        <h3>Item</h3>
+        <h3>Item</h3>
+        <h3>Item</h3>
+        <h3>Item</h3>
+      </section>
+    </main>
+  );
 };
 
 export default Cart;
